35a516d1adeb9947fb1a5b1eec6d7a8d5fe99e40
M3LiNdRu <rogercalaf@gmail.com>
2016-10-20 21:09:45 +0200

    Creating packages during build process instead if run an external script for doing it

--- GeneGenie.Gedcom/NuGet/msbuild.ps1
+++
@@ -1,6 +0,0 @@
-﻿Write-Host "Building Nuget Package with all platforms using msbuild"
-
-$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-
-Invoke-Expression $msbuild ../GeneGenie.Gedcom.csproj /t:Build /p:Configuration="Release 4.5"
-Invoke-Expression $msbuild ../GeneGenie.Gedcom.csproj /t:Build;Package;Publish /p:Configuration="Release 4.0"
