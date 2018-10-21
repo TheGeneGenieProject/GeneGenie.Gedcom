7bebea3412bb357e83f0067d69645ab207dbdcb2
RyanONeill1970 <r@ryanoneill.com>
2018-10-21 20:49:37 +0100

    Style and licence tidy up.

--- GeneGenie.Gedcom.Tests/Date/GedcomDatePeriodTest.cs
+++ GeneGenie.Gedcom.Tests/Date/GedcomDatePeriodTest.cs
@@ -1,18 +1,6 @@
 // <copyright file="GedcomDatePeriodTest.cs" company="GeneGenie.com">
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see http:www.gnu.org/licenses/ .
-//
+// Copyright (c) GeneGenie.com. All Rights Reserved.
+// Licensed under the GNU Affero General Public License v3.0. See LICENSE in the project root for license information.
 // </copyright>
 // <author> Copyright (C) 2016 Ryan O'Neill r@genegenie.com </author>
 
