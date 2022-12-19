63973d67fd9968d1e5897595e579599d63f2fc94
Ryan O'Neill <r@ryanoneill.com>
2022-12-19 11:31:01 +0000

    Rework test.

--- GeneGenie.Gedcom.Tests/Date/GedcomDateMatchTest.cs
+++ GeneGenie.Gedcom.Tests/Date/GedcomDateMatchTest.cs
@@ -5,7 +5,6 @@
 
 namespace GeneGenie.Gedcom.Date.Tests
 {
-    using System.Collections.Generic;
     using Xunit;
 
     /// <summary>
@@ -13,24 +12,12 @@ namespace GeneGenie.Gedcom.Date.Tests
     /// </summary>
     public class GedcomDateMatchTest
     {
-        public static IEnumerable<object[]> DatesToMatch()
-        {
-            yield return new object[] { string.Empty, string.Empty, 100m };
-            yield return new object[] { "19 APR 1996", "19 APR 1996", 100m };
-            yield return new object[] { "Jan 1990", "Jan 1990", 100m };
-            yield return new object[] { "Feb 2000", "FEB 2000", 100m };
-            yield return new object[] { "Jan 1 1990", "Jan 2 1990", 83.3m };
-        }
-
-        private static GedcomDate CreateDate(string dateText)
-        {
-            var date = new GedcomDate();
-            date.ParseDateString(dateText);
-            return date;
-        }
-
         [Theory]
-        [MemberData(nameof(DatesToMatch))]
+        [InlineData("", "", 100)]
+        [InlineData("19 APR 1996", "19 APR 1996", 100)]
+        [InlineData("Jan 1990", "Jan 1990", 100)]
+        [InlineData("Feb 2000", "FEB 2000", 100)]
+        [InlineData("Jan 1 1990", "Jan 2 1990", 83.3)]
         private void Dates_should_match(string dateAText, string dateBText, decimal expectedMatch)
         {
             var dateA = CreateDate(dateAText);
@@ -39,6 +26,13 @@ namespace GeneGenie.Gedcom.Date.Tests
             var matched = dateA.CalculateSimilarityScore(dateB);
 
             Assert.Equal(expectedMatch, matched, 1);
+        }
+
+        private static GedcomDate CreateDate(string dateText)
+        {
+            var date = new GedcomDate();
+            date.ParseDateString(dateText);
+            return date;
         }
     }
 }
