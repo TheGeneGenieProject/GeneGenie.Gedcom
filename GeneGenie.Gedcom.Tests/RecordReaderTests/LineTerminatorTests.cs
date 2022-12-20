f8daf71b1e757de088a4075119eb55bc654d0959
Ryan O'Neill <r@ryanoneill.com>
2022-12-20 16:18:11 +0000

    Fix another broken cross platform linefeed test.

--- GeneGenie.Gedcom.Tests/RecordReaderTests/LineTerminatorTests.cs
+++ GeneGenie.Gedcom.Tests/RecordReaderTests/LineTerminatorTests.cs
@@ -6,6 +6,7 @@
 namespace GeneGenie.Gedcom.Tests.RecordReaderTests
 {
     using GeneGenie.Gedcom.Parser;
+    using System;
     using System.IO;
     using System.Text;
     using Xunit;
@@ -21,7 +22,6 @@ namespace GeneGenie.Gedcom.Tests.RecordReaderTests
         /// <param name="text"></param>
         /// <param name="expectedTerminator"></param>
         [Theory]
-        [InlineData("", "\r\n")]
         [InlineData("First line. \n Second line.", "\n")]
         [InlineData("First line. \r Second line.", "\r")]
         public void Line_delimiter_can_be_parsed(string text, string expectedTerminator)
@@ -33,6 +33,22 @@ namespace GeneGenie.Gedcom.Tests.RecordReaderTests
                 var newlineTerminator = GedcomRecordReader.DetectNewline(sr);
 
                 Assert.Equal(expectedTerminator, newlineTerminator);
+            }
+        }
+
+        /// <summary>
+        /// Tests that an if a line terminator is not found in the text, the default system newline is used.
+        /// </summary>
+        [Fact]
+        public void System_default_line_delimiter_is_used()
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes("")))
+            {
+                var sr = new StreamReader(ms);
+
+                var newlineTerminator = GedcomRecordReader.DetectNewline(sr);
+
+                Assert.Equal(Environment.NewLine, newlineTerminator);
             }
         }
     }
